6218a787890e2355068c4d287a8e8905cde604e7
vioo-bkp <59666442+vioo-bkp@users.noreply.github.com>
2023-11-04 15:54:24 +0200

    minor updates

---
+++ leetcode/30-days/2626. Array Reduce Transformation.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/array-reduce-transformation/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    
+};
+
